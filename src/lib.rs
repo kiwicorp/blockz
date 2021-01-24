18c96c4c39393ac51d51e1724c532383d954b05f
cezarmathe <me@cezarmathe.com>
2021-01-24 19:51:17 +0200

    blockz: Add and document ComponentExt

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@
 
 pub use blockz_derive;
 
+pub mod component;
 pub mod configuration;
 pub mod singleton;
 
 /// Prelude for blockz.
 pub mod prelude {
+    pub use crate::component::ComponentExt;
     pub use crate::configuration::Configuration;
     pub use crate::singleton::Singleton;
 }
