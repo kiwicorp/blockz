da2f25f88d994b69df0780dac635edd8cdade7ad
cezarmathe <me@cezarmathe.com>
2021-02-12 09:19:20 +0200

    blockz: Add features in Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 pub use blockz_derive as derive;
 
 pub mod component;
+#[cfg(feature = "configuration")]
 pub mod configuration;
+#[cfg(feature = "singleton")]
 pub mod singleton;
 
 /// Prelude for blockz.
