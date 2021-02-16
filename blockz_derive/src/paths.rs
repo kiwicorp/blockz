23a0475d1d4c85a83cc954555aaa9f8ed139686e
cezarmathe <me@cezarmathe.com>
2021-02-16 10:56:20 +0200

    blockz_derive: Allow dead code in paths module

--- blockz_derive/src/paths.rs
+++ blockz_derive/src/paths.rs
@@ -1,65 +1,67 @@
 //! Paths to libraries used by the expansion of blockz proc-macros.
 
+#![allow(dead_code)]
+
 use proc_macro2::TokenStream;
 
 use quote::quote;
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn anyhow_path() -> TokenStream {
+pub(crate) fn anyhow_path() -> TokenStream {
     quote! { anyhow }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn anyhow_path() -> TokenStream {
+pub(crate) fn anyhow_path() -> TokenStream {
     quote! { ::anyhow }
 }
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn blockz_path() -> TokenStream {
+pub(crate) fn blockz_path() -> TokenStream {
     quote! { blockz }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn blockz_path() -> TokenStream {
+pub(crate) fn blockz_path() -> TokenStream {
     quote! { ::blockz }
 }
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn envy_path() -> TokenStream {
+pub(crate) fn envy_path() -> TokenStream {
     quote! { envy }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn envy_path() -> TokenStream {
+pub(crate) fn envy_path() -> TokenStream {
     quote! { ::envy }
 }
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn once_cell_path() -> TokenStream {
+pub(crate) fn once_cell_path() -> TokenStream {
     quote! { once_cell }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn once_cell_path() -> TokenStream {
+pub(crate) fn once_cell_path() -> TokenStream {
     quote! { ::once_cell }
 }
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn tokio_path() -> TokenStream {
+pub(crate) fn tokio_path() -> TokenStream {
     quote! { tokio }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn tokio_path() -> TokenStream {
+pub(crate) fn tokio_path() -> TokenStream {
     quote! { ::tokio }
 }
 
 #[cfg(feature = "no_absolute_paths")]
-pub fn std_path() -> TokenStream {
+pub(crate) fn std_path() -> TokenStream {
     quote! { std }
 }
 
 #[cfg(not(feature = "no_absolute_paths"))]
-pub fn std_path() -> TokenStream {
+pub(crate) fn std_path() -> TokenStream {
     quote! { ::std }
 }
