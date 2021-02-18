13e4d56e21d1a628ee5ae2718c94467fc2fa36ea
cezarmathe <me@cezarmathe.com>
2021-02-18 11:50:42 +0200

    blockz_derive: Work on #[singleton_fn_with_arg]

--- blockz_derive/src/singleton/singleton_fns.rs
+++ blockz_derive/src/singleton/singleton_fns.rs
@@ -5,10 +5,16 @@ use proc_macro2::TokenStream;
 
 use quote::quote;
 
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
 use syn::Block;
 use syn::FnArg;
 use syn::Ident;
 use syn::ItemFn;
+use syn::Pat;
+use syn::Stmt;
+use syn::Type;
+use syn::TypeTuple;
 use syn::Visibility;
 
 /// Prefix for an impl fn used by a singleton fn.
@@ -50,6 +56,91 @@ pub(super) fn impl_singleton_fn_mut(base: &ItemFn) -> ItemFn {
     impl_fn
 }
 
+/// Implement a SingletonFnWithArg.
+pub(super) fn impl_singleton_fn_with_arg(base: &ItemFn) -> ItemFn {
+    // clone the base
+    let mut impl_fn = base.clone();
+    // make the impl fn private
+    impl_fn.vis = Visibility::Inherited;
+    // rename the function
+    rename_fn(
+        &mut impl_fn,
+        format!(
+            "{}{}",
+            SINGLETON_FN_WITH_ARG_PREFIX,
+            base.sig.ident.to_string()
+        ),
+    );
+    // get the tuple type that represents the args
+    let arg_type = fn_inputs_to_type_tuple(
+        base.sig
+            .inputs
+            .iter()
+            .filter(|arg| {
+                if let FnArg::Receiver(_) = arg {
+                    false
+                } else {
+                    true
+                }
+            })
+            .collect::<Vec<&FnArg>>()
+            .as_slice(),
+    );
+    // // create the args pattern
+    // let arg_pat: Pat = syn::parse2(quote! {args: #arg_type}).expect(
+    //     format!(
+    //         "Failed to parse singleton fn args pattern: {}",
+    //         quote! {args: #arg_type}
+    //     )
+    //     .as_str(),
+    // );
+    // let arg_pat = if let Pat::Type(val) = arg_pat {
+    //     val
+    // } else {
+    //     panic!("Failed to parse singleton fn type pattern!")
+    // };
+    // fix the fn inputs
+    fn_fix_inputs(&mut impl_fn, &arg_type);
+    // fix the fn block
+    impl_fn.block = Box::new(fn_fix_block(
+        *base.block.clone(),
+        base.sig
+            .inputs
+            .iter()
+            .filter(|arg| {
+                if let FnArg::Receiver(_) = arg {
+                    false
+                } else {
+                    true
+                }
+            })
+            .collect::<Vec<&FnArg>>()
+            .as_slice(),
+        "args", // don't harcode(although it's the same as when making arg_pat)
+    ));
+    // return the impl fn
+    impl_fn
+}
+
+/// Implement a SingletonFnWithArg.
+pub(super) fn impl_singleton_fn_mut_with_arg(base: &ItemFn) -> ItemFn {
+    // clone the base
+    let mut impl_fn = base.clone();
+    // make the impl fn private
+    impl_fn.vis = Visibility::Inherited;
+    // rename the function
+    rename_fn(
+        &mut impl_fn,
+        format!(
+            "{}{}",
+            SINGLETON_FN_MUT_WITH_ARG_PREFIX,
+            base.sig.ident.to_string()
+        ),
+    );
+    // return the impl fn
+    impl_fn
+}
+
 /// Implement the facade for a SingletonFn.
 pub(super) fn impl_singleton_fn_facade(base: &ItemFn, impl_fn: &ItemFn) -> ItemFn {
     // clone the base
@@ -86,6 +177,48 @@ pub(super) fn impl_singleton_fn_mut_facade(base: &ItemFn, impl_fn: &ItemFn) -> I
             #[allow(unused_imports)]
             use blockz::singleton::Singleton;
             Self::use_mut_singleton(Self::#impl_fn_ident).await
+        },
+    );
+    // return the facade fn
+    facade_fn
+}
+
+/// Implement the facade for a SingletonFnWithArg.
+pub(super) fn impl_singleton_fn_with_arg_facade(base: &ItemFn, impl_fn: &ItemFn) -> ItemFn {
+    // clone the base
+    let mut facade_fn = base.clone();
+    // remove the receiver
+    remove_fn_receiver(&mut facade_fn);
+    // get impl fn ident
+    let impl_fn_ident = &impl_fn.sig.ident;
+    // replace the function impl
+    replace_fn_block(
+        &mut facade_fn,
+        quote! {
+            #[allow(unused_imports)]
+            use blockz::singleton::Singleton;
+            Self::use_singleton_with_arg(Self::#impl_fn_ident, (other)).await // remove hardcoding
+        },
+    );
+    // return the facade fn
+    facade_fn
+}
+
+/// Implement the facade for a SingletonFnWithArg.
+pub(super) fn impl_singleton_fn_mut_with_arg_facade(base: &ItemFn, impl_fn: &ItemFn) -> ItemFn {
+    // clone the base
+    let mut facade_fn = base.clone();
+    // remove the receiver
+    remove_fn_receiver(&mut facade_fn);
+    // get impl fn ident
+    let impl_fn_ident = &impl_fn.sig.ident;
+    // replace the function impl
+    replace_fn_block(
+        &mut facade_fn,
+        quote! {
+            #[allow(unused_imports)]
+            use blockz::singleton::Singleton;
+            Self::use_singleton(Self::#impl_fn_ident).await
         },
     );
     // return the facade fn
@@ -136,4 +269,86 @@ fn replace_fn_block(function: &mut ItemFn, block: TokenStream) {
     })
     .expect(format!("Failed to parse replacement block: {}", block).as_str());
     function.block = Box::new(block);
+}
+
+/// Convert the args of a function into a tuple and a map for mapping old types
+/// to tuple variants.
+fn fn_inputs_to_type_tuple(src: &[&FnArg]) -> TypeTuple {
+    let elems: Punctuated<Type, Comma> = src
+        .iter()
+        .map(|elem| {
+            if let FnArg::Typed(val) = elem {
+                *val.ty.clone()
+            } else {
+                panic!("Cannot convert receiver to type for tuple!")
+            }
+        })
+        .collect();
+
+    // panic!("Elements: {}", quote!{ #elems });
+
+    TypeTuple {
+        paren_token: syn::token::Paren {
+            span: Span::call_site(),
+        },
+        elems,
+    }
+}
+
+/// Fix the inputs of a function.
+///
+/// Replaces all non-receiver args by a single tuple.
+fn fn_fix_inputs(src: &mut ItemFn, tuple: &TypeTuple) {
+    let tuple_arg: FnArg = syn::parse2(quote!{args: #tuple}).expect("Failed to parse new fn inputs.");
+    // if let FnArg::Receiver(_) = tuple {
+    //     panic!("Tuple arg must not be a receiver!");
+    // }
+    let receiver = src
+        .sig
+        .inputs
+        .first()
+        .cloned()
+        .expect("Expected the fn receiver!");
+    src.sig.inputs.clear();
+    src.sig.inputs.push(receiver);
+    src.sig.inputs.push(tuple_arg);
+}
+
+/// Replace all inputs in the block with an element of the tuple arg.
+fn fn_fix_block(block: Block, src: &[&FnArg], tuple_name: &str) -> Block {
+    let args_str = src
+        .iter()
+        .map(|arg| {
+            if let FnArg::Typed(val) = arg { val } else { panic!("fn arg must not be receiver") }
+        })
+        .map(|arg| *arg.pat.clone() )
+        .enumerate()
+        .map(|(i, arg)| {
+            (
+                format!("{}", quote! {#arg}),
+                format!("{} . {}", tuple_name, i),
+            )
+        })
+        .collect::<Vec<(String, String)>>();
+    // let stmts: Vec<Stmt> = block
+    //     .stmts
+    //     .iter()
+    //     .cloned()
+    //     .map(|stmt| {
+    //         let mut stmt_str = format!("{}", quote! {#stmt});
+    //         args_str.iter().for_each(|arg| {
+    //             stmt_str = stmt_str.replace(arg.0.as_str(), arg.1.as_str());
+    //         });
+    //         syn::parse_str::<Stmt>(stmt_str.as_str()).expect("Failed to parse mapped statement!")
+    //     })
+    //     .collect();
+    // syn::parse_str::<Block>(
+    //     stmts.join(';')
+    // )
+    // .expect("Failed to parse block!")
+    let mut block_str = format!("{}", quote! {#block});
+    args_str.iter().for_each(|arg| {
+        block_str = block_str.replace(arg.0.as_str(), arg.1.as_str());
+    });
+    syn::parse_str::<Block>(block_str.as_str()).expect("Failed to parse block!")
 }
