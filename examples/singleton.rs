f23048594a19bd3b3d157e3f0af3601083040f68
cezarmathe <me@cezarmathe.com>
2021-03-05 11:02:34 +0200

    blockz: Update example singletons
    Added example for both rwlock and mutex singletons.

--- examples/singleton.rs
+++ examples/singleton.rs
@@ -8,9 +8,42 @@ use std::sync::Arc;
 
 #[derive(Singleton)]
 #[singleton(lock = "rwlock")]
-struct Dummy(Vec<i32>);
+struct DummyRwLock(Vec<i32>);
 
-impl Dummy {
+impl DummyRwLock {
+    #[singleton_fn]
+    pub async fn is_vec_empty(&self) -> anyhow::Result<bool> {
+        Ok(self.0.is_empty())
+    }
+
+    #[singleton_fn]
+    async fn clear(&mut self) -> anyhow::Result<()> {
+        self.0.clear();
+        Ok(())
+    }
+
+    #[singleton_fn]
+    pub async fn get_set(&mut self, get_index: usize, set: i32) -> anyhow::Result<Option<i32>> {
+        let get_value = self.0.get(get_index).map(|val| *val);
+        if let Some(_) = get_value {
+            *self.0.get_mut(get_index).unwrap() = set;
+            Ok(get_value)
+        } else {
+            Ok(None)
+        }
+    }
+
+    #[singleton_fn]
+    pub async fn check_equals(&self, other: Arc<Vec<i32>>) -> anyhow::Result<bool> {
+        Ok(self.0.as_slice() == *other)
+    }
+}
+
+#[derive(Singleton)]
+#[singleton(lock = "mutex")]
+struct DummyMutex(Vec<i32>);
+
+impl DummyMutex {
     #[singleton_fn]
     pub async fn is_vec_empty(&self) -> anyhow::Result<bool> {
         Ok(self.0.is_empty())
@@ -41,46 +74,81 @@ impl Dummy {
 
 #[tokio::main]
 pub async fn main() {
-    Dummy::init_singleton(Dummy(vec![-5, 3])).unwrap();
+    DummyMutex::init_singleton(DummyMutex(vec![-5, 3])).unwrap();
+    DummyRwLock::init_singleton(DummyRwLock(vec![-5, 3])).unwrap();
 
     assert!(
-        !Dummy::is_vec_empty()
+        !DummyMutex::is_vec_empty()
             .await
             .expect("Failed to check if vec is empty!"),
-        "Dummy should not have had an empty Vec!"
+        "DummyMutex should not have had an empty Vec!"
+    );
+    assert!(
+        !DummyRwLock::is_vec_empty()
+            .await
+            .expect("Failed to check if vec is empty!"),
+        "DummyRwLock should not have had an empty Vec!"
     );
 
     let arr = Arc::new(vec![-5, 3]);
     assert!(
-        Dummy::check_equals(arr.clone())
+        DummyMutex::check_equals(arr.clone())
             .await
             .expect("Failed to check if two vecs are equal"),
-        "Dummy should have had a vec that is equal to [-5, 3]!"
+        "DummyMutex should have had a vec that is equal to [-5, 3]!"
+    );
+    assert!(
+        DummyRwLock::check_equals(arr.clone())
+            .await
+            .expect("Failed to check if two vecs are equal"),
+        "DummyRwLock should have had a vec that is equal to [-5, 3]!"
     );
 
-    let get = Dummy::get_set(2 as usize, 3 as i32)
+    let get = DummyMutex::get_set(2 as usize, 3 as i32)
         .await
-        .expect("Failed to do Dummy::get_set");
+        .expect("Failed to do DummyMutex::get_set");
+    assert!(get.is_none());
+    let get = DummyRwLock::get_set(2 as usize, 3 as i32)
+        .await
+        .expect("Failed to do DummyRwLock::get_set");
     assert!(get.is_none());
 
-    let get = Dummy::get_set(0 as usize, -4 as i32)
+    let get = DummyMutex::get_set(0 as usize, -4 as i32)
         .await
-        .expect("Failed to do Dummy::get_set");
+        .expect("Failed to do DummyMutex::get_set");
+    assert_eq!(get, Some(-5));
+    let get = DummyRwLock::get_set(0 as usize, -4 as i32)
+        .await
+        .expect("Failed to do DummyRwLock::get_set");
     assert_eq!(get, Some(-5));
 
     let arr = Arc::new(vec![-4, 3]);
     assert!(
-        Dummy::check_equals(arr.clone())
+        DummyMutex::check_equals(arr.clone())
             .await
             .expect("Failed to check if two vecs are equal"),
-        "Dummy should have had a vec that is equal to [-4, 3]!"
+        "DummyMutex should have had a vec that is equal to [-4, 3]!"
+    );
+    let arr = Arc::new(vec![-4, 3]);
+    assert!(
+        DummyRwLock::check_equals(arr.clone())
+            .await
+            .expect("Failed to check if two vecs are equal"),
+        "DummyRwLock should have had a vec that is equal to [-4, 3]!"
     );
 
-    Dummy::clear().await.expect("Failed to clear Dummy!");
+    DummyMutex::clear().await.expect("Failed to clear DummyMutex!");
     assert!(
-        Dummy::is_vec_empty()
+        DummyMutex::is_vec_empty()
             .await
             .expect("Failed to check if vec is empty!"),
-        "Dummy should've had an empty Vec!"
+        "DummyMutex should've had an empty Vec!"
+    );
+    DummyRwLock::clear().await.expect("Failed to clear DummyRwLock!");
+    assert!(
+        DummyRwLock::is_vec_empty()
+            .await
+            .expect("Failed to check if vec is empty!"),
+        "DummyRwLock should've had an empty Vec!"
     );
 }
