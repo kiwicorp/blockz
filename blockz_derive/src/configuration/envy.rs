bf43ed5987baf23143f1674f1f57aaf9c93e9aae
cezarmathe <me@cezarmathe.com>
2021-03-13 12:27:55 +0200

    derive: Implement envy configuration factory
    Removed prefix_source, default_prefix and default_prefix_source for the
    time being.

--- blockz_derive/src/configuration/envy.rs
+++ blockz_derive/src/configuration/envy.rs
@@ -7,6 +7,7 @@ use proc_macro2::TokenStream;
 
 use quote::quote;
 
+use crate::common;
 use crate::paths;
 
 /// Factory that builds a Configuration implementation based on envy.
@@ -19,13 +20,7 @@ pub(super) struct EnvyConfigurationFactory<'f> {
 #[derive(Default, FromMeta)]
 pub(super) struct EnvyConfigurationOpts {
     #[darling(default)]
-    prefix: Option<String>, // fixme 12/03/21: implement the prefix option
-    #[darling(default)]
-    prefix_source: Option<String>, // fixme 12/03/21: implement the prefix_source option
-    #[darling(default)]
-    default_prefix: Option<String>, // fixme 12/03/21: implement the default_prefix option
-    #[darling(default)]
-    default_prefix_source: Option<String>, // fixme 12/03/21: implement the default_prefix_source option
+    prefix: Option<String>,
 }
 
 impl<'f> EnvyConfigurationFactory<'f> {
@@ -34,10 +29,38 @@ impl<'f> EnvyConfigurationFactory<'f> {
         Self { type_name, opts }
     }
 
+    fn get_configuration_impl_opts(&self) -> TokenStream {
+        // gather paths to dependencies
+        let blockz = paths::blockz_path();
+
+        let type_name = self.type_name;
+        let default_opts = quote! {
+            <#blockz::configuration::EnvyConfiguration<#type_name> as #blockz::configuration::Configuration>::Opts
+        };
+
+        if let Some(_) = &self.opts.prefix {
+            quote! { () }
+        } else {
+            default_opts
+        }
+    }
+
+    fn get_configuration_impl_load_arg(&self) -> TokenStream {
+        if let Some(prefix) = &self.opts.prefix {
+            let lit_prefix = common::create_lit_str(prefix.clone());
+            quote! { Some(#lit_prefix.to_string()) }
+        } else {
+            quote! { opts }
+        }
+    }
+
     /// Build the envy configuration trait implementation.
     pub fn build(self) -> TokenStream {
         // gather paths to dependencies
         let blockz = paths::blockz_path();
+
+        let opts = self.get_configuration_impl_opts();
+        let load_arg = self.get_configuration_impl_load_arg();
 
         // return the implementation
         let type_name = self.type_name;
@@ -45,11 +68,11 @@ impl<'f> EnvyConfigurationFactory<'f> {
             #[async_trait::async_trait]
             impl #blockz::configuration::Configuration for #type_name {
                 type Inner = #type_name;
-                type Opts = <#blockz::configuration::EnvyConfiguration<#type_name> as #blockz::configuration::Configuration>::Opts;
+                type Opts = #opts;
                 type Error = <#blockz::configuration::EnvyConfiguration<#type_name> as #blockz::configuration::Configuration>::Error;
 
                 async fn load(opts: Self::Opts) -> Result<Self::Inner, Self::Error> {
-                    <#blockz::configuration::EnvyConfiguration<#type_name> as #blockz::configuration::Configuration>::load(opts).await
+                    <#blockz::configuration::EnvyConfiguration<#type_name> as #blockz::configuration::Configuration>::load(#load_arg).await
                 }
             }
         }
