80df3373c3beba05ca2b3d4bcd25316bc79facde
cezarmathe <me@cezarmathe.com>
2021-03-13 12:30:17 +0200

    blockz: Adapt configuration example

--- examples/configuration.rs
+++ examples/configuration.rs
@@ -7,21 +7,24 @@ use blockz::prelude::*;
 
 use serde::Deserialize;
 
-#[derive(Configuration, Deserialize)]
-#[configuration(envy(
-    prefix = "PREFIX_",
-    prefix_source = "MY_CONSTANT",
-    default_prefix = "AB",
-    default_prefix_source = "abc()",
-))]
+#[derive(Configuration, Deserialize, PartialEq)]
+#[configuration(envy(prefix = "PREFIX_"))]
 struct MyEnvConfig {
+    server_port: u32,
+}
+
+#[derive(Configuration, Deserialize, PartialEq)]
+struct OtherEnvConfig {
     server_port: u32,
 }
 
 #[tokio::main]
 async fn main() {
-    <MyEnvConfig as EasyConfiguration>::load().await.unwrap();
-    <MyEnvConfig as Configuration>::load(Some("PREFIX_".to_string()))
-        .await
-        .unwrap();
+    let conf1 = <MyEnvConfig as EasyConfiguration>::load().await.unwrap();
+    let conf2 = <MyEnvConfig as Configuration>::load(()).await.unwrap();
+    assert!(conf1 == conf2);
+
+    let conf1 = <OtherEnvConfig as EasyConfiguration>::load().await.unwrap();
+    let conf2 = <OtherEnvConfig as Configuration>::load(None).await.unwrap();
+    assert!(conf1 == conf2);
 }
