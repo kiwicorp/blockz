cad13a41a7194dc22ec59cd49fb1d8026a886860
cezarmathe <me@cezarmathe.com>
2021-03-23 22:26:56 +0200

    derive: Add a reusable factory

--- blockz_derive/src/factory.rs
+++ blockz_derive/src/factory.rs
@@ -8,3 +8,23 @@ pub(crate) trait Factory {
     /// Build the product.
     fn build(self) -> Self::Product;
 }
+
+/// A factory that can be reused (by taking &mut self instead of self).
+pub(crate) trait ReusableFactory {
+    /// The product produced by this factory.
+    type Product;
+
+    /// Build the product.
+    fn build(&mut self) -> Self::Product;
+}
+
+impl<Rf> Factory for Rf
+where
+    Rf: ReusableFactory
+{
+    type Product = Rf::Product;
+
+    fn build(self) -> Self::Product {
+        <Rf as ReusableFactory>::build(&mut self)
+    }
+}
