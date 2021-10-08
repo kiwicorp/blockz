28571907e9de37f0fe56a85796cebb2e36345280
Armand-Cezar Mathe <me@cezarmathe.com>
2021-10-08 11:25:51 +0300

    config: Add custom error type

--- blockz-config/src/lib.rs
+++ blockz-config/src/lib.rs
@@ -1,17 +1,65 @@
 //! Convenient application configuration.
 
+use std::error::Error;
+use std::fmt;
+use std::fmt::Debug;
+use std::fmt::Display;
+
+use serde::de::DeserializeOwned;
+
+/// Result type for app configurations.
+pub type Result<T> = std::result::Result<T, AppConfigError<T>>;
+
 /// An application configuration.
-pub trait AppConfig<'a>: serde::Deserialize<'a> {
-    const NAME: &'static str;
+pub trait AppConfig: DeserializeOwned + Debug {
+    const PACKAGE: &'static str;
 
     /// Load the configuration.
-    fn load() -> Self {
-        let provider = figment::providers::Env::prefixed(Self::NAME);
-        match figment::Figment::new()
-            .join(provider)
-            .extract() {
-            Ok(value) => value,
-            Err(e) => panic!("{}: app configuration: failed to load: {}", Self::NAME, e),
+    fn load() -> Result<Self> {
+        let provider = figment::providers::Env::prefixed(Self::PACKAGE);
+        match figment::Figment::new().join(provider).extract() {
+            Ok(value) => Ok(value),
+            Err(e) => Err(AppConfigError::new(e)),
         }
+    }
+}
+
+/// Error that can be returned by the `AppConfig` trait.
+#[derive(Debug)]
+pub struct AppConfigError<T> {
+    inner: figment::Error,
+    _phantom: std::marker::PhantomData<T>,
+}
+
+impl<T> AppConfigError<T> {
+    pub(crate) fn new(inner: figment::Error) -> Self {
+        Self {
+            inner,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<T: AppConfig> Display for AppConfigError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            f.write_fmt(format_args!(
+                "{}: app config error: {:#}",
+                T::PACKAGE,
+                self.inner
+            ))
+        } else {
+            f.write_fmt(format_args!(
+                "{}: app config error: {}",
+                T::PACKAGE,
+                self.inner
+            ))
+        }
+    }
+}
+
+impl<T: AppConfig> Error for AppConfigError<T> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.inner)
     }
 }
